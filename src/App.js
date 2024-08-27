b179994aed17a50e532b0c07c0371b560256560e
Rakesh <mummanarakesh@gmail.com>
2024-08-27 23:54:00 +0530

    new2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
  
   return (
     <div >
-        <HashRouter basename='cse-1'>
+        <HashRouter>
         <Routes>
          <Route path='/'  element= {<Home></Home>}/>
           <Route path='/rows'  element= {<DisplayAssignment> </DisplayAssignment>}/>
